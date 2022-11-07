0887803752b059aa39715dd98de053141f0acb6e
Mankirat Singh <mankirat95@gmail.com>
2022-11-07 18:04:46 +0530

    Minor change

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import react from "react";
 function App() {
-    return <div className="App">THis is a Food manager app</div>;
+    return <div className="App">2THis is a Food manager app</div>;
 }
 
 export default App;
