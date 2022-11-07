893e4fb642592acdd57867a2816dab870fede342
Mankirat Singh <mankirat95@gmail.com>
2022-11-07 22:27:56 +0530

    Carousel For the Main Page

---
+++ src/UI/Carousel.js
@@ -0,0 +1,12 @@
+import React from "react";
+import backImg from "../assets/back.jpg";
+import { CarouselImage } from "../CustomStyle";
+const Carousel = (props) => {
+    return (
+        <div>
+            <CarouselImage src={backImg} alt="" srcset="" />
+        </div>
+    );
+};
+
+export default Carousel;
