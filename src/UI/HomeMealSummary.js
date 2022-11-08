b37989e7ecc471e435e9c13349e1e7dab5d85249
Mankirat Singh <mankirat95@gmail.com>
2022-11-08 22:11:32 +0530

    New component HomeMealSummary Added

---
+++ src/UI/HomeMealSummary.js
@@ -0,0 +1,26 @@
+import React from "react";
+import { HomeMealSummaryContainer } from "../CustomStyle";
+const HomeMealSummary = () => {
+    return (
+        <>
+            <HomeMealSummaryContainer>
+                <div>
+                    <h1>Delicious Food, Delivered to You</h1>
+                </div>
+
+                <div>
+                    Lorem ipsum, dolor sit amet consectetur adipisicing elit.
+                    Non soluta optio doloremque in voluptatibus, minus labore
+                    officia deleniti quas nobis saepe, quos consequuntur
+                    distinctio fugiat blanditiis vero illo hic earum! Lorem
+                    ipsum dolor, sit amet consectetur adipisicing elit.
+                    Reiciendis quis repudiandae, expedita nam soluta mollitia,
+                    laboriosam cum cupiditate corporis quisquam delectus beatae
+                    voluptates, eius pariatur iure
+                </div>
+            </HomeMealSummaryContainer>
+        </>
+    );
+};
+
+export default HomeMealSummary;
