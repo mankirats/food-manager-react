3c3e504e61609a8bf587b82633725ccba87cdba0
Mankirat Singh <mankirat95@gmail.com>
2022-11-08 22:12:47 +0530

    App.js Integrated with new component HomeMealSummary

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import Header from "./Layout/Header";
 import Carousel from "./UI/Carousel";
+import HomeMealSummary from "./UI/HomeMealSummary";
 function App() {
     return (
         <div className="App">
             <Header></Header>
             <Carousel></Carousel>
+            <HomeMealSummary></HomeMealSummary>
         </div>
     );
 }
