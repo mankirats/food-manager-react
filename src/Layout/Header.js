40e9e9a8eb6a9d7878172e4c1bf1d0116bcd4b0a
Mankirat Singh <mankirat95@gmail.com>
2022-11-15 22:10:30 +0530

    Header Updated

--- src/Layout/Header.js
+++ src/Layout/Header.js
@@ -1,10 +1,10 @@
 import HeaderCart from "../Cart/HeaderCart";
-import { Container, HeaderContainer, Logo } from "../CustomStyle";
+import { Container, HeaderContainer, Logo } from "../styles/Header.style";
 const Header = (props) => {
     return (
         <HeaderContainer>
             <Logo>Logo</Logo>
-            <Container>
+            <Container width="14rem" justify="space-between">
                 <HeaderCart>Cart</HeaderCart>
                 <span>Account</span>
             </Container>
