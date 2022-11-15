2b45a285e406db3b1f8ca06cc9a65c52a256f733
Mankirat Singh <mankirat95@gmail.com>
2022-11-15 22:12:17 +0530

    HeaderMealSummary Updated

--- src/UI/HomeMealSummary.js
+++ src/UI/HomeMealSummary.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { HomeMealSummaryContainer } from "../CustomStyle";
+import { HomeMealSummaryContainer } from "../styles/CustomStyle";
 const HomeMealSummary = () => {
     return (
         <>
